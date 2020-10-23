885aaacc8d7d4951e80aeaa625876db8e010a03b
Stephen Brasel <wearyroad@gmail.com>
2020-10-23 15:07:56 -0400

    Initial commit

---
+++ src/test/java/com/revature/SpringBootTest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
